e4f82a4d6fa0f729ba7b6c9d783d297d9f2ee29d
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-11-27 19:08:02 +0100

    Update csv_writer.py

--- csv_writer.py
+++ csv_writer.py
@@ -1,18 +1,12 @@
 #Contiene la clase PizzaCSVWriter que se encarga de escribir detalles de las pizzas en un archivo CSV.
+# csv_writer.py
 import csv
 
 class PizzaCSVWriter:
     def __init__(self, file_path):
         self.file_path = file_path
 
-    def write_pizza_to_csv(self, pizza):
+    def write_item_to_csv(self, item):
         with open(self.file_path, mode='a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([
-                pizza.tipo_masa,
-                pizza.salsa,
-                ', '.join(pizza.ingredientes_principales),
-                pizza.tecnicas_coccion,
-                pizza.presentacion,
-                pizza.maridaje_recomendado
-            ])
+            writer.writerow([item.nombre, item.precio])
